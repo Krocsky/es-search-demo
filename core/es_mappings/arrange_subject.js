9d8efcd20b9b733f3f3df466bdf8eb1d0db9fffb
Krocsky <1164994684@qq.com>
2018-10-17 19:26:59 +0800

    fix search weight problem

--- core/es_mappings/arrange_subject.js
+++ core/es_mappings/arrange_subject.js
@@ -1,14 +1,14 @@
 let arrangeSubjectModel = {
   'arrange': {
     'properties': {
-      'sub_name': { 'type': 'text', 'analyzer': 'ik_max_word', 'boost': 1 },
-      'sub_description': { 'type': 'text', 'analyzer': 'ik_max_word', 'boost': 1 },
+      'sub_name': { 'type': 'text', 'analyzer': 'ik_max_word', 'search_analyzer': 'ik_max_word' },
+      'sub_description': { 'type': 'text', 'analyzer': 'ik_max_word', 'search_analyzer': 'ik_max_word' },
       'sub_total_minutes': { 'type': 'integer' },
       'sub_is_needed': { 'type': 'boolean' },
       'sub_begin_time': { 'type': 'date', 'format': 'dateOptionalTime' },
       'sub_end_time': { 'type': 'date', 'format': 'dateOptionalTime' },
-      'teacher_name': { type: 'text', 'analyzer': 'ik_max_word' },
-      'teacher_description': { 'type': 'text', 'analyzer': 'ik_max_word', 'boost': 1 },
+      'teacher_name': { type: 'text', 'analyzer': 'ik_max_word', 'search_analyzer': 'ik_max_word' },
+      'teacher_description': { 'type': 'text', 'analyzer': 'ik_max_word', 'search_analyzer': 'ik_max_word' },
       'on_subject_at': { 'type': 'date', 'format': 'dateOptionalTime' },
     },
   },
